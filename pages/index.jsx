d314c94cfb8a1810aa08f9e34e99e97579bde185
golovin-mv <golovin.work@yandex.ru>
2020-03-30 04:28:30 +1000

    WIP

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 
-const socket = io();
+const PORT = process.env.PORT || 3000;
+
+const socket = io('/');
 socket.emit('ready');
 
 export default () => {
